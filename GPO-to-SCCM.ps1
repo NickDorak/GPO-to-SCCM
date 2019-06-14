150a718a2a71003579227e620827daa6ebf1fc2d
NickDorak <nick@dorak.ca>
2019-06-14 09:32:37 -0400

    Create GPO-to-SCCM.ps1

---
+++ GPO-to-SCCM.ps1
@@ -0,0 +1,9 @@
+# Convert GPO to SCCM CI
+
+# GPO Type (Applied GPO/Available GPO/Downloaded GPO Files)
+# Select Registry GPO
+# Select Domain/Local GPO
+# Select ADMX/ADML File
+
+# Display Detection BL
+# Display Remediation BL
